aa54955652fd7594aeed28d7cc1754f32b04add8
sholehilham-debug <sholehilham@gmail.com>
2026-03-01 10:42:40 +0700

    Update and rename pwasimpeltoko.js to dutailmu.js

--- dutailmu.js
+++ dutailmu.js
@@ -6,7 +6,7 @@ self.addEventListener("install", e => {
         "/?m=1",  // versi mobile
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
         "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap",
-        "https://blogger.googleusercontent.com/img/.../LOGO-SIMPELTOKO-Square-300x300.jpg"
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjCzD78CQTO7mKZOwB75NPRKi4SzfmDMYifleg1PFbQ_e7cgzjt1IAFpXKzwgQVW-rTxHay_YOZ7RImzH9MvIEENgbiRC3DrXG6HFEa02srIMdlsEQjm9mVseaNJnTNi4ax0WNoAtlXc0BNrgHzXLbG0liWPM7-3UmvE8YPHrR47qn_oH7F_eIJgq9bZ9dC/s320/600x600.jpg"
       ]);
     })
   );
@@ -19,3 +19,4 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
